cd7d2c1e8936761b423b5a3f046c5bab7e4ab4f3
raul <raul.hess@gmail.com>
2014-12-10 09:29:55 -0200

    [main] broke index page into several smaller files

--- index.php
+++ index.php
@@ -7,27 +7,11 @@
 <script type="text/javascript" src="js/basic.js"></script>
 </head>
 <body>
-	<div id="header">
-			<a class="mobile-link" href="#" onclick="displayMobile()">&#9776;</a>
-			<a class="main-link" href="#"><b>B</b>eholder <b>C</b>aolho</a>
-			<a class="sub-link" href="#">D&D 5ª Edição</a>
-			<a class="sub-link" href="#">Coração de Pedra</a>
-			<a class="sub-link" href="#">A Song of Ice and Fire</a>
-	</div>
-	<div id="mobile-menu">
-		<div class="menu-title">Seções</div>
-		<a class="menu-link" href="#">D&D 5ª Edição</a>
-		<a class="menu-link" href="#">Coração de Pedra</a>
-		<a class="menu-link" href="#">A Song of Ice and Fire</a>
-	</div>
-	<div id="menu">
-		<div class="menu-title">Seções</div>
-		<a class="menu-link" href="#">D&D 5ª Edição</a>
-		<a class="menu-link" href="#">Coração de Pedra</a>
-		<a class="menu-link" href="#">A Song of Ice and Fire</a>
-	</div>
-	<div id="content">
-		Hello World!
-	</div>
+	<?php
+		include 'header.html';
+		include 'mobile-menu.html';
+		include 'menu.php';
+		include 'content.php'
+	?>
 </body>
 </html>
---
+++ menu.php
@@ -0,0 +1,6 @@
+<div id="menu">
+  <div class="menu-title">Seções</div>
+  <a class="menu-link" href="#">D&D 5ª Edição</a>
+  <a class="menu-link" href="#">Coração de Pedra</a>
+  <a class="menu-link" href="#">A Song of Ice and Fire</a>
+</div>
