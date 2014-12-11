61244bb42808f82599bc0d629066b666e38ff493
raul <raul.hess@gmail.com>
2014-12-11 14:27:37 -0200

    [cdp] implemented cdp's architecture

--- cdp/content.php
+++ cdp/content.php
@@ -1,3 +1,10 @@
 <div id="content">
-  Hello World!
+  <?php
+  $page = $_GET['page'];
+  if($page != false){
+    include 'pages/'.$page.'.php';
+  }else{
+    echo "Página inicial do cenário <i>Coração de Pedra</i>.";
+  }
+  ?>
 </div>
---
+++ cdp/pages/history.php
@@ -0,0 +1,24 @@
+<div class="section">
+  <h1>Section Title</h1>
+  <p>
+    Section content
+  </p>
+</div>
+<div class="section">
+  <h1>Section Title</h1>
+  <p>
+    Section content
+  </p>
+</div>
+<div class="section">
+  <h1>Section Title</h1>
+  <p>
+    Section content
+  </p>
+</div>
+<div class="section">
+  <h1>Section Title</h1>
+  <p>
+    Section content
+  </p>
+</div>
